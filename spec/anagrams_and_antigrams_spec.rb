c888643054daf1d1427eb9fee40869b4de2cfa64
mellowmorgan <morgan.waites@gmail.com>
2021-12-10 08:21:50 -0800

    WIP: test 1 for anagram_checker method failed

--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -1,2 +1,9 @@
 require("rspec")
 require("anagrams_and_antigrams")
+
+describe('#Antigram') do
+  obj1 = Antigram.new("eat","tea")
+  it("should return 'These words are anagrams' if two words are anagrams") do
+    expect(obj1.antigram_checker).to(eql("These words are anagrams"))
+  end
+end
