8ca665570aae0d1782c114b49b88d5b5033b0967
mellowmorgan <morgan.waites@gmail.com>
2021-12-10 08:39:40 -0800

    DEBUG: test 1 anagram_checker method passes

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,1 +1,25 @@
 require("pry")
+
+class Antigram
+  attr_accessor(:word1)
+  attr_accessor(:word2)
+  
+  def initialize(word1,word2)
+    @word1 = word1
+    @word2 = word2
+  end
+
+  def antigram_checker
+    same=false
+    arr1 = @word1.split("").sort
+    arr2 = @word2.split("").sort
+    for i in 0..arr1.length-1
+      if arr1[i]==arr2[i]
+        same=true
+      else 
+        return 'These words are NOT anagrams'
+      end
+    end
+    'These words are anagrams'
+  end
+end
