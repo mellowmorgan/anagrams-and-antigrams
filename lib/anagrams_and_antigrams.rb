5a35ee2ca9192e2f650108b66164655ee709729e
mellowmorgan <morgan.waites@gmail.com>
2021-12-10 09:02:33 -0800

    helper method added to check if words are valid test passes

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -8,20 +8,40 @@ class Antigram
     @word1 = word1
     @word2 = word2
   end
+  def are_words?
+    checker1=false
+    checker2=false
+    vowels = ['a','e','i','o','u']
+    vowels.each do |vowel|
+      if @word1.include?(vowel)
+        checker1=true
+      end
+      if @word2.include?(vowel)
+        checker2=true
+      end
+    end
+    if checker1 && checker2
+      true
+    else
+      false
+    end
+  end
 
   def antigram_checker
     same=false
     arr1 = @word1.split("").sort
     arr2 = @word2.split("").sort
-    for i in 0..arr1.length-1
+    for i in 0..arr1.length-1 do
       if arr1[i]==arr2[i]
         same=true
       else 
         return 'These words are NOT anagrams'
       end
     end
-    if same && @word1.length==@word2.length
-    'These words are anagrams'
+    if same && @word1.length==@word2.length && are_words?
+      'These words are anagrams'
+    else
+      'These words are NOT anagrams'
     end
   end
 end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -5,6 +5,7 @@ describe('#Antigram') do
   obj1 = Antigram.new("eat","tea")
   obj2 = Antigram.new("ate","tea")
   obj3 = Antigram.new("ate","teak")
+  obj4 = Antigram.new("ktk","tkk")
   it("should return 'These words are anagrams' if two words are anagrams") do
     expect(obj1.antigram_checker).to(eql("These words are anagrams"))
   end
@@ -13,5 +14,11 @@ describe('#Antigram') do
   end
   it("should return 'These words are anagrams' if two words are anagrams") do
     expect(obj3.antigram_checker).to(eql("These words are NOT anagrams"))
+  end
+  it("should return 'These words are NOT anagrams' if one of words not valid") do
+    expect(obj4.antigram_checker).to(eql("These words are NOT anagrams"))
+  end
+  it("should return true") do
+    expect(obj2.are_words?).to(eql(true))
   end
 end
